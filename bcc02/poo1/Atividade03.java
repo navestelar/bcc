134095f10c21d9905d3fd7a696173cccee28f4db
unknown <acch.escola@gmail.com>
2023-08-06 17:48:41 -0300

    Atividade 03

---
+++ bcc02/poo1/Atividade03.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+    public class Atividade03 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        // //ex01
+        // System.out.print("Escreva o primeiro número: ");
+        // float n1 = input.nextFloat();
+        // System.out.print("Escreva o segundo número: ");
+        // float n2 = input.nextFloat();
+        // if (n1>n2) {
+        //     System.out.print(n1);
+        // }else {
+        //     System.out.print(n2);
+        // }
+        // System.out.println(" é o maior.");
+
+        // //ex02
+        // System.out.print("Escreva um número: ");
+        // float n = input.nextFloat();
+        // System.out.print("O número "+n+" é ");
+        // if (n>0){
+        //     System.out.println("positivo.");
+        // }else if (n<0){
+        // System.out.println("negativo.");
+        // }else {
+        // System.out.println("nulo.");
+        // }
+        
+        // //ex03
+        // System.out.print("Escreva M ou F: ");
+        // String letra = input.nextLine();
+		// if (letra.toUpperCase().equals("F")){
+		// 	System.out.println("Feminino");
+		// }else if (letra.toUpperCase().equals("M")){
+		// 	System.out.println("Masculino");
+        // }
+
+        // //ex04
+        // System.out.print("Ano: ");
+        // int year = input.nextInt();
+        // if (year%4==0){
+        //     System.out.println("Bissexto");
+        // }else {
+        //     System.out.println("Não bissexto");
+        // }
+
+        // input.close();
+
+        // //ex05
+        // System.out.print("Letra: ");
+        // String letter = input.nextLine();
+        // if (letter.equals("a") || letter.equals("e") || letter.equals("i") || letter.equals("o") || letter.equals("u")) {
+        //     System.out.println("Vogal");
+        // }else {
+        //     System.out.println("Consoante");
+        // }
+
+        input.close();
+    }
+}
