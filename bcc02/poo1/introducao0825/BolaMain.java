7280aecb0011a033ab9bd7e9c8b58b25f9f09f73
Ana Carolina Capistrano Huntemann <92680998+navestelar@users.noreply.github.com>
2023-09-01 10:19:27 -0300

    Update BolaMain.java

--- bcc02/poo1/introducao0825/BolaMain.java
+++ bcc02/poo1/introducao0825/BolaMain.java
@@ -1,17 +1,20 @@
 public class BolaMain {
     public static void main(String[] args) {
         Bola bola1 = new Bola();
-        //modo1(evitar)
-        // bola1.tamanho=10.5f;
-        // bola1.tipo="Futebol";
-        // System.out.println(bola1.tamanho);
-        // System.out.println(bola1.tipo);
+        // //modo1(evitar)
+        // // bola1.tamanho=10.5f;
+        // // bola1.tipo="Futebol";
+        // // System.out.println(bola1.tamanho);
+        // // System.out.println(bola1.tipo);
         
-        // modo2
-        bola1.setTamanho(10.5f);
-        bola1.setTipo("Futebol");
-        System.out.println(bola1.getTamanho());
-        System.out.println(bola1.getTipo());
-        
+        // // modo2
+        // bola1.setTamanho(10.5f);
+        // bola1.setTipo("Futebol");
+        // System.out.println(bola1.getTamanho());
+        // System.out.println(bola1.getTipo());
+
+        bola1.setPeso(10);
+        bola1.setTamanho(50);
+        bola1.setTipo("Volei");
     }
 }
