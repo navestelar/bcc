9e12041f2cd889d9d44a66f7058547f6321b7184
Ana Carolina Capistrano Huntemann <92680998+navestelar@users.noreply.github.com>
2023-09-01 10:16:09 -0300

    Update Bola.java

--- bcc02/poo1/introducao0825/Bola.java
+++ bcc02/poo1/introducao0825/Bola.java
@@ -1,30 +1,60 @@
 public class Bola {
-    //modo1 (evitar)
-    // float tamanho;
-    // String tipo;
-    private float tamanho=100;
-    private String tipo="Futebol";
+    // //modo1 (evitar)
+    // // float tamanho;
+    // // String tipo;
+    
+    // private float tamanho=100;
+    // private String tipo="Futebol";
     
     
+    // public void setTamanho(float tamanho) {
+    //     if(tamanho>0){
+    //         this.tamanho = tamanho;
+    //     }
+    // }
+    // public void setTipo(String tipo) {
+    //     if(tipo.length()>0){
+    //         this.tipo = tipo;
+    //     }
+    // }
+    // public float getTamanho() {
+    //     return this.tamanho;
+    // }
+    // public String getTipo() {
+    //     return this.tipo;
+    // }
+    
+    // // public Bola(float tamanho, String tipo) {
+    // //     this.tamanho = tamanho;
+    // //     this.tipo = tipo;
+    // // }
+
+    private String esporte = "Futebol";
+    private float tamanho = 100;
+    private float peso = 5.2f;
+
     public void setTamanho(float tamanho) {
         if(tamanho>0){
             this.tamanho = tamanho;
         }
     }
-    public void setTipo(String tipo) {
-        if(tipo.length()>0){
-            this.tipo = tipo;
+    public void setTipo(String esporte) {
+        if(esporte.length()>0){
+            this.esporte = esporte;
+        }
+    }
+    public void setPeso(float peso) {
+        if(peso>0){
+            this.peso = peso;
         }
     }
     public float getTamanho() {
-        return this.tamanho;
+        return tamanho;
     }
     public String getTipo() {
-        return this.tipo;
+        return esporte;
     }
-    // public Bola(float tamanho, String tipo) {
-    //     this.tamanho = tamanho;
-    //     this.tipo = tipo;
-    // }
-
+    public float getPeso() {
+        return peso;
+    }
 }
