3a1ca6e7ac741fa0de67347074ab6005040080d7
Ana Carolina Capistrano Huntemann <92680998+navestelar@users.noreply.github.com>
2023-09-01 10:38:22 -0300

    Create Cachorro.java

---
+++ bcc02/poo1/introducao0825/Cachorro.java
@@ -0,0 +1,31 @@
+public class Cachorro {
+    private float peso;
+    private String raca;
+    private float tamanho;
+    private String nome;
+    
+    public String getRaca() {
+        return raca;
+    }
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+    public float getTamanho() {
+        return tamanho;
+    }
+    public void setTamanho(float tamanho) {
+        this.tamanho = tamanho;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public float getPeso() {
+        return peso;
+    }
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+}
